ce43b8e0ebc10bd18454d3e73f0d61d2e29b2217
Hui Gao <45728873+hongyingxuying@users.noreply.github.com>
2026-03-16 16:49:01 +0800

    Add files via upload

---
+++ model_graph/__init__.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+"""
+@author: HanJinZhe
+@project: GNNforIFD
+@file: __init__.py.py
+@time: 2023/4/17 9:21
+@description： 
+"""
+from model_graph.GAT import GAT
+from model_graph.GCN import GCN
